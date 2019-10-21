6453929be409170d4614b79793576b309fed0b3d
BK Singh <54300707+bksinghtech@users.noreply.github.com>
2019-10-21 17:36:58 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,7 +28,7 @@ Any Problems Massage Me @YourX
 Our Best Friend @bksinghback @bksinghyt
 For more commands click /help...
 
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+**Please Join Our Channel And Group.**
 
 """
 
